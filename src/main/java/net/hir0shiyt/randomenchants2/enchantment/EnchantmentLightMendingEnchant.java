df7bf28b32955f5f5e4e9b8c142f161fc21a827b
Hir0shiyt <hiroshicrackyt@gmail.com>
2023-07-18 16:56:01 +0200

    Fixes

--- src/main/java/net/hir0shiyt/randomenchants2/enchantment/EnchantmentLightMendingEnchant.java
+++ src/main/java/net/hir0shiyt/randomenchants2/enchantment/EnchantmentLightMendingEnchant.java
@@ -1,9 +1,9 @@
 package net.hir0shiyt.randomenchants2.enchantment;
 
 import net.hir0shiyt.randomenchants2.RandomEnchants2;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.LightLayer;
@@ -12,14 +12,19 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ObjectHolder;
 
+
 @Mod.EventBusSubscriber(modid = RandomEnchants2.MOD_ID)
-public class EnchantmentSolar extends Enchantment {
-    @ObjectHolder(RandomEnchants2.MOD_ID + "light_mending_enchant")
+public class EnchantmentLightMendingEnchant extends Enchantment {
+    @ObjectHolder(RandomEnchants2.MOD_ID + ":light_mending_enchant")
     public static final Enchantment LIGHT_MENDING_ENCHANT = null;
 
-    public EnchantmentSolar() {
+    public EnchantmentLightMendingEnchant() {
         super(Rarity.VERY_RARE, EnchantmentCategory.BREAKABLE, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
         this.setRegistryName(RandomEnchants2.MOD_ID, "light_mending_enchant");
+    }
+
+    protected EnchantmentLightMendingEnchant(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots) {
+        super(pRarity, pCategory, pApplicableSlots);
     }
 
     @Override
@@ -33,14 +38,15 @@ public class EnchantmentSolar extends Enchantment {
     }
 
     @SubscribeEvent
-    public static void applySolar(TickEvent.PlayerTickEvent event) {
-        if (event.phase == TickEvent.Phase.START || event.player.level.isClientSide())
+    public static void applyLightMending(TickEvent.PlayerTickEvent event) {
+        if (event.phase != TickEvent.Phase.START || event.player.level.isClientSide())
             return;
+
         for (EquipmentSlot slot : EquipmentSlot.values()) {
             ItemStack stack = event.player.getItemBySlot(slot);
             if (!stack.isEmpty() && stack.isDamaged()) {
                 int level = EnchantmentHelper.getItemEnchantmentLevel(LIGHT_MENDING_ENCHANT, stack);
-                if (level > 0 && event.player.level.getBrightness(LightLayer.BLOCK) >= 15) {
+                if (level > 0 && event.player.level.getBrightness(LightLayer.BLOCK, event.player.blockPosition()) >= 15) {
                     int repairAmount = level * 2;
                     stack.setDamageValue(Math.max(0, stack.getDamageValue() - repairAmount));
                 }
--- src/main/java/net/hir0shiyt/randomenchants2/enchantment/ModEnchantments.java
+++ src/main/java/net/hir0shiyt/randomenchants2/enchantment/ModEnchantments.java
@@ -1,25 +1,21 @@
 package net.hir0shiyt.randomenchants2.enchantment;
 
 import net.hir0shiyt.randomenchants2.RandomEnchants2;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.enchantment.Enchantment;
 
+
+@Mod.EventBusSubscriber(modid = RandomEnchants2.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEnchantments {
     public static final DeferredRegister<Enchantment> ENCHANTMENTS =
             DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, RandomEnchants2.MOD_ID);
 
-    public static RegistryObject<Enchantment> LIGHT_MENDING_ENCHANTMENT =
-            ENCHANTMENTS.register("light_mending_enchantment",
-                    () -> new LightMendingEnchantment(Enchantment.Rarity.VERY_RARE,
-                            EnchantmentCategory.BREAKABLE, EquipmentSlot.MAINHAND,
-                            CreativeModeTab.TAB_MISC));
-
+    public static final RegistryObject<Enchantment> LIGHT_MENDING_ENCHANTMENT =
+            ENCHANTMENTS.register("light_mending_enchant", EnchantmentLightMendingEnchant::new);
 
     public static void register(IEventBus eventBus) {
         ENCHANTMENTS.register(eventBus);
